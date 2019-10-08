06fefd1356807b538a7d8b2cab5b204684777e41
YUJING GAO <gao.yujing.csu@gmail.com>
2019-10-08 23:00:35 +0000

    commit

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ app.get('/', function (req, res){
     res.send("hello world");
 });
 
-app.get('/about', function){
+app.get('/about', function(req,res){
     res.send("<h1>about page</h1>");
-}
+});
 
 app.listen(port, function(){
 
