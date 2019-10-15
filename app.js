d349b6741222ed6a90c0c358b06d8309c496c9f5
pebblestad <stadtmueller.johnathan@yahoo.com>
2019-10-15 22:36:55 +0000

    bug fix

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
+var http = require('http');
+var path = require('path');
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", 'ejs');
+
 app.get('/', function (req, res){
-    res.send("hello world");
+    res.render("index");
 });
 
 app.get('/about', function(req,res){
     res.send("<h1>about page</h1>");
 });
 
-app.listen(port, function(){
+http.createServer(app).listen(port, function(){
 
 });
